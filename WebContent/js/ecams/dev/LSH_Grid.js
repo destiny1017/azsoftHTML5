add2bd2bde0a638b31b706fd5b2115ef48b4b04b
Iryn3829 <shlee@azsoft.kr>
2019-03-12 13:19:08 +0900

    중복처리 커밋

--- WebContent/js/ecams/dev/LSH_Grid.js
+++ WebContent/js/ecams/dev/LSH_Grid.js
@@ -33,14 +33,29 @@ function Cmd_Ip_Click(temp){
 	if(temp == '1'){
 		for(var i = 0 ; i < firstGrid.selectedDataIndexs.length ; i++){
 			var selectIndex = firstGrid.selectedDataIndexs[i];			
-			firstGrid.list[selectIndex].colorsw = "3";	// 선택 데이터 컬럼색상 변경
-			secondGrid.addRow($.extend({}, firstGrid.list[selectIndex], {__index: undefined}));
+			// 중복데이터 추가안하는 로직
+			if(firstGrid.list[selectIndex].colorsw != "3"){
+				secondGrid.addRow($.extend({}, firstGrid.list[selectIndex], {__index: undefined}));
+				firstGrid.list[selectIndex].colorsw = "3";	// 선택 데이터 컬럼색상 변경
+			}
 		}
-		firstGrid.setData(firstGrid.list);	// 선택 데이터 컬럼색상 변경하기 위한 그리드 새로 그리기
+		firstGrid.repaint();	// 선택 데이터 컬럼색상 변경하기 위한 그리드 새로 그리기
 		firstGrid.clearSelect();
 		secondGrid.clearSelect();
 	} else if(temp == '2') {
+		// 추가한 데이터 삭제시 firstGrid의 데이터 색상 돌려놓기위한 로직 시작
+		for(var i = 0 ; i < secondGrid.selectedDataIndexs.length ; i++){
+			var selectIndex2 = secondGrid.selectedDataIndexs[i];
+			for(var z = 0; z < firstGrid.list.length ; z++){
+				if(firstGrid.list[z].cm_maname == secondGrid.list[selectIndex2].cm_maname){
+					firstGrid.list[z].colorsw = "";
+				}
+			}
+		}
+		// 추가한 데이터 삭제시 firstGrid의 데이터 색상 돌려놓기위한 로직 끝
+		
 		secondGrid.removeRow("selected"); // row 삭제
+		firstGrid.repaint();	// 선택 데이터 컬럼색상 변경하기 위한 그리드 새로 그리기
 		firstGrid.clearSelect();
 		secondGrid.clearSelect();
 	}
@@ -67,25 +82,15 @@ function setGrid(){
         	    this.self.repaint();
         	},
         	trStyleClass: function () {
-        		/*
-        		secondGrid.forEach( function(requestGridItem, requestGridItemIndex) {
-            		console.log(fileGridItem.cm_filename);
-            		console.log(this.item.cm_filename);
-					if(fileGridItem.cm_filename == this.item.cm_filename){
-						fileGridItem.selected_flag = '1';
-						return "fontStyle-error";
-					}
-				});
-				*/
         		if (this.item.colorsw === '3'){
         			return "fontStyle-cncl";
         		} else {
         		}
-        	},
+        	}
         },
         columns: [
-            {key: "cm_maname", label: "메뉴명",  width: '30%'},
-            {key: "colorsw", label: "색상",  width: '30%'},
+            {key: "cm_maname", label: "메뉴명",  width: '20%'},
+            {key: "colorsw", label: "색상",  width: '10%'},
             {key: "cm_filename", label: "링크파일명",  width: '70%'} //formatter: function(){	return "<button>" + this.value + "</button>"}, 	 
         ]
     });
